4003e11af506b3cfe94d40ce265fc585e2aab335
Giancarlos Gutiérrez Cervantes <ggutierrez@canvia.com>
2023-07-24 17:50:37 -0500

    ÃAuthorities applied to user

--- src/main/java/com/ggutierrez/pizzeria/service/UserSecurityService.java
+++ src/main/java/com/ggutierrez/pizzeria/service/UserSecurityService.java
@@ -4,11 +4,16 @@ import com.ggutierrez.pizzeria.persistence.entity.UserEntity;
 import com.ggutierrez.pizzeria.persistence.entity.UserRoleEntity;
 import com.ggutierrez.pizzeria.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserSecurityService implements UserDetailsService {
@@ -33,7 +38,28 @@ public class UserSecurityService implements UserDetailsService {
                 .password(userEntity.getPassword())
                 .accountLocked(userEntity.getLocked())
                 .disabled(userEntity.getDisabled())
-                .roles(roles)
+                .authorities(this.grantedAuthorities(roles))
+//                .roles(roles)
                 .build();
+    }
+
+    private String[] getAuthorities(String role) {
+        if (role.equals("ADMIN") || role.equals("CUSTOMER")) {
+            return new String[] {"random_order"};
+        }
+        return new String[]{};
+    }
+
+    private List<GrantedAuthority> grantedAuthorities(String[] roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+
+        for (String role: roles) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+
+            for (String authority: this.getAuthorities(role)) {
+                authorities.add(new SimpleGrantedAuthority(authority));
+            }
+        }
+        return authorities;
     }
 }
