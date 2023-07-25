b23fb9f4d6d6f05017570ca351fdff545d0d156b
Giancarlos Gutiérrez Cervantes <ggutierrez@canvia.com>
2023-07-24 19:26:35 -0500

    feat: Method security enabled

--- src/main/java/com/ggutierrez/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/ggutierrez/pizzeria/web/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,12 +19,14 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeHttpRequests -> {
             authorizeHttpRequests
+                    .requestMatchers("/api/customers/**").hasAnyRole("ADMIN", "CUSTOMER")
                     .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
                     .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
                     .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
--- src/main/java/com/ggutierrez/pizzeria/web/controller/CustomerController.java
+++ src/main/java/com/ggutierrez/pizzeria/web/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package com.ggutierrez.pizzeria.web.controller;
 
 import com.ggutierrez.pizzeria.persistence.entity.CustomerEntity;
+import com.ggutierrez.pizzeria.persistence.entity.OrderEntity;
 import com.ggutierrez.pizzeria.service.CustomerService;
+import com.ggutierrez.pizzeria.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +11,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
     private final CustomerService customerService;
+    private final OrderService orderService;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, OrderService orderService) {
         this.customerService = customerService;
+        this.orderService = orderService;
     }
 
     @GetMapping("/phone/{phone}")
     public ResponseEntity<CustomerEntity> getByPhone(@PathVariable String phone) {
         return ResponseEntity.ok(this.customerService.getByPhone(phone));
+    }
+
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<List<OrderEntity>> getCustomerOrders(@PathVariable("id") String idCustomer) {
+        return ResponseEntity.ok(this.orderService.getCustomerOrders(idCustomer));
     }
 }
